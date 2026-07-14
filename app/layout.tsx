03f0beaf0f10ae5ebaf667145b09640dfc0b773e
ttang0825 <ttang0825@gmail.com>
2026-07-14 10:33:19 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
               <Link href="/admin/quotes">견적 관리</Link>
               <Link href="/admin/orders">운송오더</Link>
               <Link href="/admin/drivers">차주 관리</Link>
+              <Link href="/admin/dispatches">배차 관리</Link>
             </nav>
           </div>
         </div>
