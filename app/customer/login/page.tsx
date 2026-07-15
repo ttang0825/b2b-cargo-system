f894f416abac34eba1d1a1c8cbc1c26adbb0d5cb
ttang0825 <ttang0825@gmail.com>
2026-07-16 08:48:05 +0900

    Update page.tsx

--- app/customer/login/page.tsx
+++ app/customer/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { supabaseCustomer as supabase } from "@/lib/supabaseCustomerClient";
 
 export default function CustomerLoginPage() {
   const router = useRouter();
