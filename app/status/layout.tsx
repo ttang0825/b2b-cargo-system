f1cb64f23447a6e9c7b39951ee06b21c5efa41f7
ttang0825 <ttang0825@gmail.com>
2026-07-22 15:59:27 +0900

    Create layout.tsx

---
+++ app/status/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "문의·신청 현황 조회 | WeCarry 운송",
+};
+
+export default function StatusLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
